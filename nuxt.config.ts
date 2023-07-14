10bff985210e0dc06eff84d71f159a72c371db12
SamanSah <sahraei.saman91@gmail.com>
2023-07-14 17:40:34 +0330

    set cookie and handle error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
+  runtimeConfig : {
+    public: {
+      WEATHER_APP_SECRET: process.env.WEATHER_APP_SECRET
+    }    
+  },
+
   css: [
     "~/assets/css/tailwind.css"
   ],
